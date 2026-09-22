d7a6803c5b641c896fb524e279bac73edcbc1b1a
hope <hoiwujhb@student.wethinkcode.co.za>
2026-09-22 16:53:00 +0200

    updated App.jsx adding footer.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      
+      <Footer />
     </BrowserRouter>
   )
 }
