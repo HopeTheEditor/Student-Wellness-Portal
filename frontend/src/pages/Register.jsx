37be7ed5ed4164d32ca74269aca591dadae727f7
hope <hoiwujhb@student.wethinkcode.co.za>
2026-09-22 16:47:05 +0200

    updated Register.jsx & App.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,19 +1,37 @@
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
+    <main>
+      <h1>Create an Account</h1>
 
       <p>Create your student account.</p>
 
       <form>
         <label>Full Name</label>
+        <br />
         <input type="text" />
+
+        <br />
+        <br />
+
+        <label>Email</label>
+        <br />
+        <input type="email" />
+
+        <br />
+        <br />
+
+        <label>Password</label>
+        <br />
+        <input type="password" />
+
+        <br />
+        <br />
 
         <button type="submit">
           Register
         </button>
       </form>
-    </div>
+    </main>
   )
 }
 
